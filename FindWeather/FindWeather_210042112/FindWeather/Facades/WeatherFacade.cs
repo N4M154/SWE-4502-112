a74c453a73737b273bc23a15e2154d87264da50d
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-20 10:08:46 +0600

    access modifier facade

--- FindWeather/FindWeather_210042112/FindWeather/Facades/WeatherFacade.cs
+++ FindWeather/FindWeather_210042112/FindWeather/Facades/WeatherFacade.cs
@@ -8,9 +8,9 @@ namespace FindWeather
 {
     public class WeatherFacade
     {
-        private readonly WeatherProviderProxy weatherStackProxy;
-        private readonly WeatherProviderProxy openWeatherProxy;
-        private (double Latitude, double Longitude)? cachedLocation = null;
+        public WeatherProviderProxy weatherStackProxy;
+        public WeatherProviderProxy openWeatherProxy;
+        public (double Latitude, double Longitude)? cachedLocation = null;
 
         public WeatherFacade()
         {
