7a7fe5a65b55e589b3b3209fe3ba02b377a8de49
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-20 08:58:47 +0600

    weatherstack & openweather

--- FindWeather/FindWeather_210042112/FindWeather/APIs/WeatherStackBase.cs
+++ FindWeather/FindWeather_210042112/FindWeather/APIs/WeatherStackBase.cs
@@ -12,6 +12,10 @@ namespace FindWeather.APIs
         private readonly string apiKey = "1d5774fb918f8d1301086452e73b086b";
         private readonly HttpClient client = new HttpClient();
 
-        
+        public async Task<string> FetchWeatherDataAsync(double latitude, double longitude)
+        {
+            string url = $"http://api.weatherstack.com/current?access_key={apiKey}&query={latitude},{longitude}";
+            return await client.GetStringAsync(url);
+        }
     }
 }
