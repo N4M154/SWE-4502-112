b7ced3e2cd00fdb3be7c76931afd4d6bb3653245
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-20 09:23:42 +0600

    openweather adapter

---
+++ FindWeather/FindWeather_210042112/FindWeather/Adapters/OpenWeatherAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FindWeather
+{
+    public class OpenWeatherAdapter:IWeatherProvider
+    {
+        private readonly OpenWeatherBase baseClient = new OpenWeatherBase();
+        public async Task<WeatherData> GetWeatherAsync(double latitude, double longitude)
+        {
+            var response = await baseClient.FetchWeatherDataByCoordinatesAsync(latitude, longitude);
+            var json = JsonDocument.Parse(response);
+
+            
+        }
+
+    }
+}
