d2e80ae8f71196020385e9ce4f7c2d49ed7c636a
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-20 09:46:29 +0600

    proxy function

---
+++ FindWeather/FindWeather_210042112/FindWeather/Proxies/WeatherProviderProxy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindWeather
+{
+    public class WeatherProviderProxy : IWeatherProvider
+    {
+        private readonly IWeatherProvider provider;
+        private readonly Dictionary<string, (WeatherData, DateTime)> cache = new Dictionary<string, (WeatherData, DateTime)>();
+        private DateTime lastRequestTime = DateTime.MinValue;
+    }
+}
