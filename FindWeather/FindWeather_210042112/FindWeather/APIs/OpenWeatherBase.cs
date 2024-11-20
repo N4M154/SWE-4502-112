616b5fcc2ecb56e5d76379952a72c9fb920a23e6
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-20 08:57:28 +0600

    openweather

---
+++ FindWeather/FindWeather_210042112/FindWeather/APIs/OpenWeatherBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindWeather.APIs
+{
+    public class OpenWeatherBase
+    {
+        private readonly string apiKey = "6478730e0f11b84d687c7b57123b8fbf";
+        private readonly HttpClient client = new HttpClient();
+
+        
+    }
+}
