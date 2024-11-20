8ae07ddd5297643a6da9dca041f4501411fd1239
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-20 10:06:40 +0600

    facade maybe

---
+++ FindWeather/FindWeather_210042112/FindWeather/Facades/WeatherFacade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindWeather
+{
+    public class WeatherFacade
+    {
+        private readonly WeatherProviderProxy weatherStackProxy;
+        private readonly WeatherProviderProxy openWeatherProxy;
+        private (double Latitude, double Longitude)? cachedLocation = null;
+    }
+}
