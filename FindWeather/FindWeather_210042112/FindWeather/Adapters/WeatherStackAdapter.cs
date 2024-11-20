718e1768ad989cdee8d8ddd7b000c6920cdf619f
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-20 09:06:13 +0600

    adapters

---
+++ FindWeather/FindWeather_210042112/FindWeather/Adapters/WeatherStackAdapter.cs
@@ -0,0 +1,23 @@
+﻿using FindWeather.APIs;
+using FindWeather.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindWeather.Adapters
+{
+    public class WeatherStackAdapter:IWeatherProvider
+    {
+        private readonly WeatherStackBase baseClient = new WeatherStackBase();
+        public async Task<WeatherData> GetWeatherAsync(double latitude, double longitude)
+        {
+            var response = await baseClient.FetchWeatherDataAsync(latitude, longitude);
+            var json = JsonDocument.Parse(response);
+            
+        }
+
+        
+    }
+}
