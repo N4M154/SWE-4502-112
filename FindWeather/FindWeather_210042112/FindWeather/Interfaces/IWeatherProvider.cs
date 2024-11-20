531965d0fe8be93e15f68594ef9b9ff956f25fa7
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-20 08:25:51 +0600

    interface

---
+++ FindWeather/FindWeather_210042112/FindWeather/Interfaces/IWeatherProvider.cs
@@ -0,0 +1,15 @@
+﻿using FindWeather.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindWeather.Interfaces
+{
+    public interface IWeatherProvider
+    {
+        Task<WeatherData> GetWeatherAsync(double latitude, double longitude);
+        Task<WeatherData> GetWeatherByCityAsync(string city);
+    }
+}
