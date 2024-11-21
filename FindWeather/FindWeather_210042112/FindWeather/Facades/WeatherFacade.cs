350bbef8e6d302e3fdc0f077b298426880810b31
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-21 17:43:12 +0600

    facade cache

--- FindWeather/FindWeather_210042112/FindWeather/Facades/WeatherFacade.cs
+++ FindWeather/FindWeather_210042112/FindWeather/Facades/WeatherFacade.cs
@@ -31,12 +31,19 @@ namespace FindWeather
 
         public async Task<(double Latitude, double Longitude)> GetLocationAsync()
         {
+            if(cachedLocation.HasValue)
+            {
+                Console.WriteLine("Using cached data...);
+                return cachedLocation.Value;
+            }
             string ip = await new HttpClient().GetStringAsync("https://api.ipify.org");
             string url = $"http://api.ipstack.com/{ip}?access_key=de9240a0520081a9ce9e78214ec3e707";
             var response = await new HttpClient().GetStringAsync(url);
             var json = JsonDocument.Parse(response);
             double latitude = json.RootElement.GetProperty("latitude").GetDouble();
             double longitude = json.RootElement.GetProperty("longitude").GetDouble();
+
+            cachedLocation = (latitude, longitude);                   
             return (latitude, longitude);
         }
 
